24ea16ef4aa4642be73cfa375b179dc0ac55595d
S-exypig <1321987303@qq.com>
2023-07-12 23:05:15 +0800

    2023/7/12 23:05

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -6,25 +6,21 @@ using namespace std;
 // 1.寻找第一个大于 target 的元素的下标,不存在返回-1
 int upper(vector<int>& arr, int target) {
     int left = 0, right = arr.size() - 1;
+    // 特殊情况
+    if (arr[right] <= target) return -1;
     // 迭代法
-    while (left <= right) {  // 先找target
+    // 此模板为找到最后一个target的下一个元素
+    while (left < right) {
         int mid = left + (right - left) / 2;
         if (arr[mid] < target) {
             left = mid + 1;
         } else if (arr[mid] > target) {
-            right = mid - 1;
-        } else {  // 找到target后找第一个大的元素
-            int res = mid;
-            while (res <= arr.size() - 1) {
-                if (arr[res] > target) {
-                    return res;
-                }
-                res++;
-            }
-            return -1;
+            right = mid;
+        } else {
+            left = mid + 1;
         }
     }
-    return -1;
+    return left;
 }
 
 // 2.如果数组中存在元素等于
@@ -33,13 +29,14 @@ int upper(vector<int>& arr, int target) {
 int floor_upper(vector<int>& arr, int target) {
     int left = 0, right = arr.size() - 1;
     // 迭代法
+    // 该模板和上一模板本质类似，都是找到target然后再进行操作
     while (left <= right) {
         int mid = left + (right - left) / 2;
         if (arr[mid] < target) {
             left = mid + 1;
         } else if (arr[mid] > target) {
             right = mid - 1;
-        } else {
+        } else {  // 重点都在于arr[mid] == target时的操作
             int res = right;
             while (res > mid) {
                 if (arr[res] == target) return res;
@@ -52,15 +49,45 @@ int floor_upper(vector<int>& arr, int target) {
 }
 // 3.寻找最后一个小于 target 的元素的下标
 // 如果都不存在，则返回-1
-int lower(vector<int>& arr, int target) {}
+int lower(vector<int>& arr, int target) {
+    int left = 0, right = arr.size() - 1;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] < target) {
+            left = mid;
+        } else if (arr[mid] > target) {
+            right = mid - 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
 // 4.如果数组中存在元素等于 target，则返回第一个等于target
 // 的下标，如果不存在，则返回最后一个小于 target 的元素的下标。
 // 如果都不存在，则返回-1
-int floor_lower(vector<int>& arr, int target) {}
+int floor_lower(vector<int>& arr, int target) {
+    int left = 0, right = arr.size() - 1;
+    if (arr[left] > target) return -1;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] < target) {
+            left = mid;
+        } else if (arr[mid] > target) {
+            right = mid - 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    if (left + 1 < arr.size() && arr[left + 1] == target) return left + 1;
+    return left;
+}
 
 int main() {
     vector<int> arr = {1, 2, 3, 3, 4, 4, 5, 5, 7};
     cout << upper(arr, 3) << endl;        // print 4
     cout << floor_upper(arr, 3) << endl;  // print 4
+    cout << lower(arr, 3) << endl;        // print 1
+    cout << floor_lower(arr, 3) << endl;  // print 2
     return 0;
 }
